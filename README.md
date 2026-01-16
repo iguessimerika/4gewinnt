c69e8cb5207c868420890ca0a98da4ab1713a55a
Erika Rohrmeyer <erika.rohrmeyer@rdf.nuernberg.de>
2026-01-16 10:22:22 +0100

    funktionen hinzugefügt

---
+++ README.md
@@ -0,0 +1,16 @@
+
+Ausgabefunktionen:
+    - player_entry()
+    - display_current_player(player)
+    - display_board(board)
+    - display_result(is_win, winner)
+    - get_choice()
+    - clear_screen()
+    - display_error(message)
+    - play_again()
+
+Spielfunktionen:
+    - generate_board()
+    - insert_circle(board, column, color)
+    - check_win(board)
+    - check_column(column)
--- main.py
+++ main.py
@@ -1,7 +1,71 @@
 import tui, game
 
 def main():
-    pass
+    while True:
+        # show the game name
+        tui.starting_screen()
+
+        # get player names and data
+        players = tui.player_entry()
+        winner = None
+        win = False
+        tie = False
+        # generate empty board as 2d array
+        board = game.generate_board()
+        
+        # display board in the terminal
+        tui.display_board(board)
+        
+        while True:
+            for player in players:
+                # get player data
+                player_data = players.get(player)
+                name = player_data.get("name")
+                color = player_data.get("color")
+                
+                tui.spacer()
+                
+                # show, whose turn it is
+                tui.display_player_turn(name, color)
+                
+                # prompt player to enter a row and column for their play
+                while True:
+                    column = tui.get_choice()
+                    
+                    # check if it was a valid position (not filled, out of bounds)
+                    if game.check_column(column, board):
+                        break
+                    else:
+                        tui.display_error()
+                
+                # update the board
+                board = game.insert_circle(board, column, color)
+                
+                # check if there are no more plays available
+                tie = game.check_tie(board)
+                
+                if not tie:
+                    # check for a win vertically, horizontally or diagonally
+                    win = game.check_win(board)
+                    
+                    tui.display_board(board)
+                    
+                    if win:
+                        winner = name
+                        break
+                else:
+                    break
+            if win or tie:
+                break
+        
+        tui.spacer()
+        
+        # show the winner or a tie
+        tui.display_result(win, winner)
+
+        # ask the user to play again
+        if not tui.play_again():
+            break
 
 
 if __name__ == "__main__":
